17a1fe2b8ea7377a67cbf79af616669098c02a3a
Slug-Boi <theis.p.holm@gmail.com>
2025-10-20 08:34:22 +0200

    move shell nix file to outer dir

--- shell.nix
+++ shell.nix
@@ -2,10 +2,10 @@
 { pkgs ? import <nixpkgs> {}}:
 
 pkgs.mkShell {
-  packages = [ pkgs.python311];
+  packages = [ pkgs.python311 ];
 
   shellHook = ''
-    echo "--* Shell and env activated *--"
+    echo "--* Shell and conda env activated *--"
   '';
 }
 
